fe8d8c2936c308f875c2aae5596872d3660f2a5d
PonikTrofik <izanami369@gmail.com>
2020-09-29 13:16:17 +0200

    chapter 4. done

--- src/index.js
+++ src/index.js
@@ -8,18 +8,50 @@ const {ApolloServer, gql} = require('apollo-server-express');
 // run server on port in .env or port 4k
 const port = process.env.PORT || 4000;
 
+let notes = [
+    {id: '1', content: 'This note', author: 'Tony'},
+    {id: '2', content: 'This is a note', author: 'Anna'},
+    {id: '3', content: 'This another note', author: 'Lebowski'}];
+
+    
 //construct schema, using GraphQL
 const typeDefs = gql`
 type Query {
-    hellnoo:String
+    hellnoo:String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+},
+type Note {
+    id: ID!
+    content: String!
+    author: String!
+},
+type Mutation {
+    newNote(content: String!): Note!
 }`;
 
 //provide resolver functions for our schema fields
 const resolvers = {
     Query:{
-        hellnoo: () => 'Hell noo worlddd !'
+        hellnoo: () => 'Hell noo worlddd !',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id ===args.id);
+        }
+    },
+    Mutation: {
+        newNote: (parent,args) => {
+            let noteValue = {
+                id: String(notes.length +1),
+                content: args.content,
+                author: 'Tonik Profik'
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }
     }
 };
+
 
 const app = express();
 
